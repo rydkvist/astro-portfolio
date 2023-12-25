7082479dd459e74dc8dde5baa86359ae7627819e
Nojze <niklas@rydkvist.com>
2023-12-25 16:27:42 +0100

    Update SVGR command in package.json

--- src/components/react/icons/MessageCircle.tsx
+++ src/components/react/icons/MessageCircle.tsx
@@ -3,7 +3,7 @@ import type { SVGProps } from "react";
 import { type Ref, forwardRef } from "react";
 const SvgMessageCircle = (
   props: SVGProps<SVGSVGElement>,
-  ref: Ref<SVGSVGElement>,
+  ref: Ref<SVGSVGElement>
 ) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
