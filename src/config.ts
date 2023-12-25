8e607dd50cabf445701b7807803426d2a1c27600
Nojze <niklas@rydkvist.com>
2023-12-25 03:26:00 +0100

    Add .prettierrc and update astro.config.mjs and SEO

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,3 @@
-export const APP_HOME_URL = "/";
-export const APP_ABOUT_URL = "/about";
-export const APP_PROJECTS_URL = "/projects";
-export const APP_INVESTMENTS_URL = "/investments";
-export const APP_MUSIC_URL = "/music";
-
-export const APP_WEBSITE_URL = "https://www.rydkvist.com";
-export const APP_TITLE_SUFFIX = "Niklas Rydkvist";
-export const APP_SLOGAN = "Niklas Rydkvist";
-export const APP_DESCRIPTION =
-  "Niklas Rydkvist is a Software Engineer based in Stockholm, Sweden. He is passionate about building software that facilitates the day-to-day life of people.";
-
-const isDevelopment = false; //process.env.VERCEL_ENV === "development";
-
-export const ABSOLUTE_WEBSITE_URL = isDevelopment
-  ? "http:/localhost:3000"
-  : "https://www.rydkvist.com";
-
 export const EXTERNAL_LINKS = {
   website: "https://www.rydkvist.com",
   github: "https://github.com/rydkvist",
@@ -24,11 +6,11 @@ export const EXTERNAL_LINKS = {
 } as const;
 
 export const APP_LINKS = {
-  home: APP_HOME_URL,
-  about: APP_ABOUT_URL,
-  projects: APP_PROJECTS_URL,
-  investments: APP_INVESTMENTS_URL,
-  music: APP_MUSIC_URL,
+  home: "/",
+  about: "/about",
+  projects: "/projects",
+  investments: "/investments",
+  music: "/music",
 } as const;
 
 export const PERSONAL = {
@@ -38,5 +20,3 @@ export const PERSONAL = {
   addressWithGoogleMaps:
     "https://maps.google.com/maps/search/Stockholm, Sweden",
 } as const;
-
-export const seo;
